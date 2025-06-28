c05b3a479d4639549790757301e786ae5b35aacd
@Jameck <lameckmpoyo0@gmil.com>
2025-06-28 22:13:48 +0200

    adding more in json file

--- App.js
+++ App.js
@@ -74,6 +74,7 @@ const styles = StyleSheet.create({
     },
     author: {
         marginBottom: 10,
+        color: '#fff',
         fontWeight: '500',
         fontFamily: 'TimesNewRoman',
         fontSize: 16,
